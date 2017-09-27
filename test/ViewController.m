d27387f1c7d254588fb28354c888f9a14a8274c6
wangwei <guolidemayilang@sina.com>
2017-09-27 18:10:51 +0800

    gggg

--- test/ViewController.m
+++ test/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 
@@ -24,6 +26,10 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (void)didReceiveMemoring {
+ 
+    
+    
+}
 
 @end
